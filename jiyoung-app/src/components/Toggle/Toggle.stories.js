3fd923a6cb4a14442550ce557119a67a71e07619
Jungjjeong <sky990106@dgu.ac.kr>
2022-05-30 14:51:38 +0900

    Feat: Spacer, Spinner, Toggle Component

---
+++ jiyoung-app/src/components/Toggle/Toggle.stories.js
@@ -0,0 +1,15 @@
+import Toggle from ".";
+
+export default {
+  title: "Component/Toggle",
+  component: Toggle,
+  argTypes: {
+    disabled: {
+      control: "boolean",
+    },
+  },
+};
+
+export const Default = (args) => {
+  return <Toggle {...args} />;
+};
