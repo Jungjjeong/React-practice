c38049f8634b9deaf69b3dcf8bb03f6a7890853c
Jungjjeong <sky990106@dgu.ac.kr>
2022-06-03 15:38:25 +0900

    Feat: useAsync, useHotKey Hooks

---
+++ jiyoung-app/src/stories/hooks/useHotKey.stories.js
@@ -0,0 +1,38 @@
+import useHotKey from "../../hooks/useHotKey";
+import { useState } from "react";
+
+export default {
+  title: "Hook/useHotKey",
+};
+
+export const Default = () => {
+  const [value, setValue] = useState("");
+  const hotkeys = [
+    {
+      global: true,
+      combo: "meta+shift+k",
+      onKeyDown: (e) => {
+        alert("meta+shift+k");
+      },
+    },
+    {
+      combo: "esc",
+      onKeyDown: (e) => {
+        setValue("");
+      },
+    },
+  ];
+
+  const { handleKeyDown } = useHotKey(hotkeys);
+
+  return (
+    <div>
+      <div>useHotKey 테스트</div>
+      <input
+        onKeyDown={handleKeyDown}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
+  );
+};
