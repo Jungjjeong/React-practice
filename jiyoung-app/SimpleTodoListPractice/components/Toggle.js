40b16dc79379833ef700fe683500de484c6fd3c3
Jungjjeong <sky990106@dgu.ac.kr>
2022-06-06 18:25:01 +0900

    Chore: TodoList 분리

--- jiyoung-app/SimpleTodoListPractice/components/Toggle.js
+++ jiyoung-app/SimpleTodoListPractice/components/Toggle.js
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import useToggle from "../hooks/useToggle";
+import useToggle from "../../src/hooks/useToggle";
 
 const ToggleContainer = styled.label`
   display: inline-block;
