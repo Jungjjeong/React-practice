cffe5a1ceb5fb54a572eead44813dd09695b6768
Jungjjeong <sky990106@dgu.ac.kr>
2022-06-06 21:28:34 +0900

    Feat: axios 실습

--- jiyoung-app/src/App.js
+++ jiyoung-app/src/App.js
@@ -1,28 +1,42 @@
 import axios from "axios";
+import { useCallback } from "react";
+import PostList from "./components/domain/PostList";
+import PostProvider from "./contexts/PostProvider";
 import { useAsync } from "./hooks";
-import { Header, Spinner } from "./components/";
+import { Header } from "./components";
+import PostAddForm from "./components/domain/PostAddForm";
 
-function App() {
-  const initialPost = useAsync(async () => {
+const App = () => {
+  const initialPosts = useAsync(async () => {
     return await axios
       .get("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.data);
   }, []);
 
+  const handleAddPost = useCallback(async (post) => {
+    console.log(post);
+    return await axios
+      .post(`https://jsonplaceholder.typicode.com/posts`, post)
+      .then((response) => response.data);
+  }, []);
+
+  const handleDeletePost = useCallback(async (id) => {
+    return await axios
+      .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(() => ({ id }));
+  }, []);
+
   return (
-    <div>
+    <PostProvider
+      initialPosts={initialPosts.value}
+      handleAddPost={handleAddPost}
+      handleDeletePost={handleDeletePost}
+    >
       <Header>Posts</Header>
-      <ul>
-        {initialPost.isLoading ? (
-          <Spinner />
-        ) : (
-          (initialPost.value || []).map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))
-        )}
-      </ul>
-    </div>
+      <PostAddForm />
+      <PostList />
+    </PostProvider>
   );
-}
+};
 
 export default App;
