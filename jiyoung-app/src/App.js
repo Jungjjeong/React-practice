9607dc254b7e7b37582c8a7c01703a7cfe923058
Jungjjeong <sky990106@dgu.ac.kr>
2022-06-06 22:57:17 +0900

    Feat: react-router 실습 (v6)

--- jiyoung-app/src/App.js
+++ jiyoung-app/src/App.js
@@ -1,41 +1,17 @@
-import axios from "axios";
-import { useCallback } from "react";
-import PostList from "./components/domain/PostList";
-import PostProvider from "./contexts/PostProvider";
-import { useAsync } from "./hooks";
-import { Header } from "./components";
-import PostAddForm from "./components/domain/PostAddForm";
+import { Routes, Route } from "react-router";
+import { PostsPage, PostPage, NotFoundPage } from "@pages";
+import DefaultTemplate from "@components/template/DefaultTemplate";
 
 const App = () => {
-  const initialPosts = useAsync(async () => {
-    return await axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.data);
-  }, []);
-
-  const handleAddPost = useCallback(async (post) => {
-    console.log(post);
-    return await axios
-      .post(`https://jsonplaceholder.typicode.com/posts`, post)
-      .then((response) => response.data);
-  }, []);
-
-  const handleDeletePost = useCallback(async (id) => {
-    return await axios
-      .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then(() => ({ id }));
-  }, []);
-
   return (
-    <PostProvider
-      initialPosts={initialPosts.value}
-      handleAddPost={handleAddPost}
-      handleDeletePost={handleDeletePost}
-    >
-      <Header>Posts</Header>
-      <PostAddForm />
-      <PostList />
-    </PostProvider>
+    <DefaultTemplate>
+      <Routes>
+        <Route path="/" exact element={<h1>Home</h1>} />
+        <Route path="/posts" exact element={<PostsPage />} />
+        <Route path="/posts/:postId" element={<PostPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </DefaultTemplate>
   );
 };
 
